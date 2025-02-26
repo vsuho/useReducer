ed5e809b3b576e0110802ae57745808bce8f7f47
vsuho <156258471+vsuho@users.noreply.github.com>
2025-02-26 20:23:03 +0200

    Todo-list using useReducer

--- components/Remove.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Button } from 'react-native'
-
-export default function Remove() {
-  return (
-     <Button title="X" color="red" onPress={() => removeTask(id)} />
-  )
-}
